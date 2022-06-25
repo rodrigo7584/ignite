13ac5711bd7a43e99e0e8106256dbd94c63c1e85
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2022-06-25 17:09:22 -0300

    25/06/2022

--- EscolaDigital/src/App.tsx
+++ EscolaDigital/src/App.tsx
@@ -1,28 +1,14 @@
-import { gql, useQuery } from "@apollo/client"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./components/Router"
+import { client } from "./lib/apollo"
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-    title
-    }
-  }
-`
-interface Lesson {
-  id:string;
-  title:string;
-}
-
-function App() {
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
-  console.log(data)
+export function App() {
   return (
-   <ul>
-    {data?.lessons.map(lesson => {
-      return <li key={lesson.id}>{lesson.title}</li>
-    })}
-   </ul>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
-
-export default App
